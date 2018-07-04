f54078e7654ce1a64e2385e8765ad3a007b8c3c9
Oliver Paraskos <Oliver.Paraskos@metapack.com>
2018-07-04 14:24:39 +0100

    Added tslint

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var sourcemaps = require('gulp-sourcemaps');
 var typescript = require('gulp-typescript');
+var tslint = require("gulp-tslint");
 
 gulp.task('clean', function () {
     gulp.src('dist/**')
@@ -11,6 +12,10 @@ gulp.task('clean', function () {
 
 gulp.task('typescript', function () {
     gulp.src('src/**/*.ts')
+        .pipe(tslint({
+            formatter: "verbose"
+        }))
+        .pipe(tslint.report())
         .pipe(sourcemaps.init())
         .pipe(typescript())
         .pipe(sourcemaps.write())
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,2 @@
-console.log("Hello World!");
+let log = console.log;
+log("Hello World!");
